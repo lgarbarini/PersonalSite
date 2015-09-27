b495650ba922f116369f1f6de6e969498b059a82
Logan Garbarini <logangarbarini@gmail.com>
2015-09-27 18:46:13 -0400

    very old site

---
+++ NHD/files/theme/local_data.js
@@ -0,0 +1,139 @@
+﻿{
+'dateTimeFormat': 'iso8601',
+'events' : [
+  {
+	'start': '1912-06-23',
+  'title': 'Birth',
+  'description': 'Alan Mathison Turing is born, son of Julius Mathison Turing and Ethel Sara Turing in London, England.',
+  },
+  {
+	'start': '1918',
+  'title': 'School',
+  'description': 'Alan is enrolled at St. Michael’s School at the age of six.'
+  },
+  {
+  'start': '1922',
+  'title': 'Prep School',
+  'description': 'Alan follows his brother John to enroll at Hazelhurst preparatory school.'
+  }, 
+  {
+  'start': '1926',
+  'title': 'Shereborne',
+  'description': 'Alan moves on to Sherborne School.'
+  },
+  {
+	'start': '1927',
+  'end': '1930',
+  'title': 'Early interest',
+  'description': 'Furthers his love of science and math by reading the current works of Einstein and others. A close friend of his, Christopher Morcom, dies, devastating Alan.'
+  },
+  {
+	'start': '1931',
+  'end': '1934',
+  'title': 'College',
+  'description': 'Studies mathematics at King’s College, Cambridge, graduating with honors.'
+  },
+  {
+	'start': '1936',
+  'title': 'Paper',
+  'description': 'Publishes “On Computable Numbers, With An Application To The Entscheidungsproblem” and introduces the Turing machine.'
+  },
+  {
+	'start': '1938',
+  'title': 'Princeton',
+  'description': 'Goes to Princeton University in America to study mathematics and is awarded his PhD.'
+  },
+  {
+	'start': '1939-09-01',
+  'title': 'Beginning of WWII',
+  'description': 'Alan is asked to join Government Codes and Ciphers School, arriving the day after the start of World War II.'
+  },
+  {
+	'start':'1941',
+  'title': 'Bombe',
+  'description': 'Gordon Welchman and Alan Turing (and others) develop the Bombe to depict the German Enigma.'
+  },
+  {
+	'start': '1943',
+  'end': '1945',
+  'title': 'Bletchley' ,
+  'description': 'Gordon Welchman and Alan Turing (and others) develop the Bombe to depict the German Enigma.'
+  },
+  {
+	'start': '1945',
+  'title': 'Post-War',
+  'description': 'Alan is awarded the OBE (Order of British Empire) for his wartime services. Konrad Zuse began work on the first algorithmic programming language, plan calculus.'
+  },
+  {
+	'start': '1948',
+  'title': 'Binary',
+  'description':'Claude Shannon´s "The Mathematical Theory of Communication"  “identified the bit as the fundamental unit of data and, coincidentally, the basic unit of computation.” - (Computer History Museum)'
+  },
+  {
+	'start': '1950',
+  'title': 'Pilot ACE',
+  'description': 'Publishes the famous “Computing Machinery and Intelligence” in which he develops the Turing Test. “Alan Turing´s philosophy directed design of Britain´s Pilot ACE at the National Physical Laboratory.” - (Computer History Museum)'
+  },
+  {
+	'start':'1951',
+  'title': 'UNIVAC',
+  'description': '“The UNIVAC I (UNIVersal Automatic Computer) delivered to the U.S. Census Bureau was the first commercial computer to attract widespread public attention.” -(Computer History Museum)'
+  },
+  {
+	'start': '1952',
+  'title': 'Arrest',
+  'description': 'Turing is arrested for gross indecency (homosexuality), thereby losing his security clearance. He is offered chemical treatment instead of jail, which proves to be extremely damaging.'
+  },
+  {
+	'start': '1952',
+  'title': 'First Compiler',
+  'description': 'Grace Hopper completes the A-0 Compiler, which uses English-like words instead of just numbers.'
+  },
+  {
+	'start':'1953',
+  'end': '1954',
+  'title': 'Final Years',
+  'description': 'Much of Turing\'s work in biology and physics is left unfinished because of his arrest. '
+  },
+  {
+	'start': '1954',
+  'title': 'Death',
+  'description': 'Turing dies in Winslow, Cheshire from self-inflicted potassium cyanide poisoning.'
+  },
+  {
+  'start':'1955',
+  'title': 'First Implementation',
+  'description': 'Turing’s early work in computation leads Herbert Simon and Allen Newell release the Logic Theorist software, which “supplied rules of reasoning and proved symbolic logic theorems." -(Computer History Museum)'
+  },
+  {
+   'start':'1956',
+   'title':'Reprogrammable Computers',
+   'description':'Bob Patrick of General Motors Research and Owen Mock of North American Aviation build on the basics of computing that Turing pioneered to create the first operating system, the GM-NAA I/O System, for the IBM 704. “MIT researchers built the TX-0, the first general-purpose, programmable computer built with transistors.” - (Computer History Museum)'
+  },
+  {
+  	'start':'1957',
+  	'title':'First Programming Language',
+  	'description':'Sperry Rand releases the commercial compiler for the UNIVAC. “A new language, FORTRAN (short for FORmula TRANslator), enabled a computer to perform a repetitive task from a single set of instructions by using loops.” - (Computer History Museum)'
+  },
+  {
+  	'start':'1958',
+  	'title':'Network',
+  	'description':'“SAGE (Semi-Automatic Ground Environment) linked hundreds of radar stations in the United States and Canada in the first large-scale computer communications network”- (Computer History Museum). SAGE linked the small-scale work that Turing and others had created.'
+  },
+  {
+  	'start':'1960',
+  	'title':'LISP',
+  	'description': '“LISP made its debut as the first computer language designed for writing artificial intelligence programs.”- (Computer History Museum) LISP took the basic and messy work on artificial intelligence that Turing had worked on and made it much more streamlined.'
+  },
+  {
+  	'start': '1962',
+  	'title': 'LINC',
+  	'description': '“The LINC (Laboratory Instrumentation Computer) offered the first real time laboratory data processing.”'
+  },
+  {
+  	'start':'1963',
+  	'title':'ASCII',
+  	'description': '“ASCII (American Standard Code for Information Interchange) permitted machines from different manufacturers to exchange data.” Turing’s ideas provide the foundation for the operations conducted by computers, leading to the age of computers and forever changing the modern world.'
+  },
+ ],
+}
